6b2928888ec4abcba9a973e93afd5f18f6200cf6
shraddha <shraddhamhatre113@gmail.com>
2023-06-06 09:36:16 +0200

    schema added

--- server/user/user.model.js
+++ server/user/user.model.js
@@ -7,18 +7,108 @@ const APIError = require('../helpers/APIError');
  * User Schema
  */
 const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true
+  id: {
+    type: String
   },
-  mobileNumber: {
-    type: String,
-    required: true,
-    match: [/^[1-9][0-9]{9}$/, 'The value of path {PATH} ({VALUE}) is not a valid mobile number.']
+  email:{
+    type:String
   },
-  createdAt: {
-    type: Date,
-    default: Date.now
+  password:{
+    type:String
+  },
+  first_name: {
+    type: String
+  },
+  last_name: {
+    type: String
+  },
+  DOB: {
+    type: Date
+  },
+  gender: {
+    type: String
+  },
+  hobbies: {
+    type: String
+  },
+  interest: {
+    type: String
+  },
+  about: {
+    type: String
+  },
+  response_time: {
+    type: String
+  },
+  picture_id: {
+    type: String
+  },
+  neighbourhood: {
+    type: String
+  },
+  response_rate: {
+    type: Number
+  },
+  is_superhost: {
+    type: Boolean
+  },
+  has_profile_pic: {
+    type: Boolean
+  },
+  identity_verified: {
+    type: Boolean
+  },
+  verifications: {
+    type: [
+      String
+    ]
+  },
+  properties: {
+    type: [
+      String
+    ]
+  },
+  property_bookings: {
+    past_booking: {
+      type: [
+        String
+      ]
+    },
+    current_bookings: {
+      type: [
+        String
+      ]
+    },
+    rejected_bookings: {
+      type: [
+        String
+      ]
+    }
+  },
+  guest_booking: {
+    past_booking: {
+      type: [
+        String
+      ]
+    },
+    current_bookings: {
+      type: [
+        String
+      ]
+    },
+    cancelled_bookings: {
+      type: [
+        String
+      ]
+    }
+  },
+  address_id: {
+    type: String
+  },
+  transactions: {
+    type: [
+      String
+    ]
   }
 });
 
