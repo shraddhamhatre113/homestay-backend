eea9304876a8c122d13798f7d35bf4ea740869b5
shraddhamhatre113 <shraddhamhatre113@gmail.com>
2023-07-13 21:56:03 +0200

    cors1

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use(compress());
 app.use(methodOverride());
 app.use(fileUpload());
 app.use(cors({
-  origin: 'https://bookme-lhvh.onrender.com/',
+  origin: 'https://bookme-lhvh.onrender.com',
   optionsSuccessStatus: 200,
   credentials: false,
   methods: ['GET', 'POST', 'PUT', 'DELETE']
